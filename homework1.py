120ca549fedc75dec5012b57b068c8da3ad3cf62
IreneZu <izuraeva@gmail.com>
2024-06-28 11:27:18 +0300

    Add files via upload
    ПЗ по курсу "Строки и индексация строк"

---
+++ homework1.py
@@ -0,0 +1,17 @@
+# 1
+example = "Добрый день!"
+# 2
+print(example[0])
+
+# 3
+print(example[-1])
+
+# 4
+l= len(example)//2
+print(example[l:])
+
+#5
+print(example[::-1])
+
+#6
+print(example[1::2])
