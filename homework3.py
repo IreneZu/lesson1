0f8181c24cd4bacb22bb013db3c16ac0d9fc2407
IreneZu <izuraeva@gmail.com>
2024-06-29 11:24:33 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,14 @@
+# Динамическая типизация
+
+name = 'Ирина'
+print('Name:', name)
+
+age = 17
+print('Age:', age)
+
+age = age + 33
+print('New Age:', age)
+
+is_student = True
+print('Is Student:', is_student)
+
